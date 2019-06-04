03d0f77a2bee12b5c27d81dbf22422563ed8f037
Elmer Thomas <elmer@thinkingserious.com>
2019-06-04 16:42:49 -0700

    Warning for existing Twilio CLI installations

--- scripts/local-test.sh
+++ scripts/local-test.sh
@@ -4,7 +4,7 @@ set -ex
 
 brew update
 brew install Formula/twilio-cli.rb
-brew link --overwrite twilio-cli
+brew link --overwrite twilio-cli # WARNING: Nukes existing cli install
 twilio version
 twilio help
 brew uninstall Formula/twilio-cli.rb
