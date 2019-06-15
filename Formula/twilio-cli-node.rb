c47100b038373d28412cc7279628bbb0b6a88d1c
Elmer Thomas <elmer@thinkingserious.com>
2019-06-15 08:14:35 -0700

    v1.2.1 update

--- Formula/twilio-cli-node.rb
+++ Formula/twilio-cli-node.rb
@@ -1,10 +1,10 @@
 class TwilioCliNode < Formula
     desc "node.js dependency for the Twilio CLI"
     homepage "https://github.com/twilio/twilio-cli"
-    url "https://github.com/twilio/homebrew-brew/releases/download/v0.0.3/node-v10.16.0.zip"
-    sha256 "22ce4285387142556012b1c8dd8ef496cbf07157075e3af9e5d368d9a507eb07"
+    url "https://twilio-cli-prod.s3.amazonaws.com/node-v10.16.0-darwin-x64.tar.gz"
+    sha256 "6c009df1b724026d84ae9a838c5b382662e30f6c5563a0995532f2bece39fa9c"
   
     def install
-        libexec.install buildpath/"node"
+        libexec.install buildpath/"bin/node"
     end
   end
