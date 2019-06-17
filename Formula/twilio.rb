60043740085c72e89a48448c1afb8b28135a557f
Elmer Thomas <ethomas@twilio.com>
2019-06-17 08:47:46 -0700

    Use versioned tarball

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio.tar.gz"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.2.1/twilio-v1.2.1.tar.gz"
   version "1.2.1"
   sha256 "294fa3c307d2374deae3b4a57cbb166897ca23e9e9784c7397b82330f19ed254"
   depends_on "twilio/brew/twilio-cli-node"
