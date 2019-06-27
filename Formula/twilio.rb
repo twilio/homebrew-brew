d146edc8720bf57e523c81af8ad54822e416036e
Elmer Thomas <ethomas@twilio.com>
2019-06-27 09:05:21 -0700

    v1.3.1 update (#5)

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.3.0/twilio-v1.3.0.tar.gz"
-  version "1.3.0"
-  sha256 "ba97726cd6101bb47130b58425f4df579866a9f4d33782ce2a58a23b8ec45471"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.3.1/twilio-v1.3.1.tar.gz"
+  version "1.3.1"
+  sha256 "a2b0c675b25225be1b2bb4f484ffabba9882b891c932d583fe64e2dc9248a085"
   depends_on "twilio/brew/twilio-cli-node"
 
   def install
@@ -16,16 +16,16 @@ class Twilio < Formula
   end
 
   def post_install
-    ohai "*************************************************************************"
-    ohai "*                                                                       *"
-    ohai "* To get started, please login to your Twilio account:                  *"
-    ohai "*                                                                       *"
-    ohai "*     twilio login                                                      *"
-    ohai "*                                                                       *"
-    ohai "*     OR                                                                *"
-    ohai "*                                                                       *"
-    ohai "*     twilio projects:add                                               *"
-    ohai "*                                                                       *"
-    ohai "*************************************************************************"
+    ohai "********************************************************"
+    ohai "*                                                      *"
+    ohai "* To get started, please login to your Twilio account: *"
+    ohai "*                                                      *"
+    ohai "*     twilio login                                     *"
+    ohai "*                                                      *"
+    ohai "*     OR                                               *"
+    ohai "*                                                      *"
+    ohai "*     twilio projects:add                              *"
+    ohai "*                                                      *"
+    ohai "********************************************************"
   end
 end
