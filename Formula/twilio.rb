5b2396969e77b95d324f42747fee69103029b69b
Kranti Talluri <ktalluri@twilio.com>
2019-07-24 12:58:35 -0700

    Release 1.3.5

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.3.4/twilio-v1.3.4.tar.gz"
-  version "1.3.4"
-  sha256 "053b3a575ca786bea755937c20d2011d409ec052526d1425331f84282fc17dbf"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.3.5/twilio-v1.3.5.tar.gz"
+  version "1.3.5"
+  sha256 "fddb2b087ffe3392a44940e54a047c8d0875eab35c7abd3b695b4017db6dfab4"
   depends_on "twilio/brew/twilio-cli-node"
 
   def install
