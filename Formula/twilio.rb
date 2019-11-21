f081b0ac68fe12f8b180ab85583ce85569d95b83
childish-sambino <sharrison@twilio.com>
2019-11-21 11:28:10 -0600

    Drop dependency on twilio-cli-node (#8)

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -6,11 +6,9 @@ class Twilio < Formula
   url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.8.2/twilio-v1.8.2.tar.gz"
   version "1.8.2"
   sha256 "3e7e1f146e6c3c164f0b6d9f49b87143b24dd1812d02fe896314cfc9ed7d44f9"
-  depends_on "twilio/brew/twilio-cli-node"
+  depends_on "node"
 
   def install
-    inreplace "bin/twilio", /^CLIENT_HOME=/, "export TWILIO_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    inreplace "bin/twilio", "\"$DIR/node\"", "#{Formula["twilio-cli-node"].libexec}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/twilio"
   end
