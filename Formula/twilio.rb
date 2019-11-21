cd32485636ca53eb540c15275119021cb346988d
Twilio <twilio-ci@twilio.com>
2019-11-21 17:48:43 +0000

    Release 1.8.3

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.8.2/twilio-v1.8.2.tar.gz"
-  version "1.8.2"
-  sha256 "3e7e1f146e6c3c164f0b6d9f49b87143b24dd1812d02fe896314cfc9ed7d44f9"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.8.3/twilio-v1.8.3.tar.gz"
+  version "1.8.3"
+  sha256 "0bcec26bc949463825b037cda8cd6276968518b7b380d2986c5f23d8a91a700f"
   depends_on "node"
 
   def install
