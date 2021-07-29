73561a188b57f5cb2ce3ef49f63706556af1b297
Twilio <team_interfaces+github@twilio.com>
2021-07-29 07:25:33 +0000

    Release 2.27.0

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v2.26.0/twilio-v2.26.0.tar.gz"
-  version "2.26.0"
-  sha256 "dd0a7cf96c2e55f5a7b8382f95732144349db3e14d602ab46a6d79ca2de61b51"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v2.27.0/twilio-v2.27.0.tar.gz"
+  version "2.27.0"
+  sha256 "92e49d8689acfb65ff8ec480c409784514a59dde3b7279106a41bc629c274f02"
   depends_on "node"
 
   def install
