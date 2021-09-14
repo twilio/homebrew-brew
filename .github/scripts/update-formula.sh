3064da666dd8c248ebaeb96a926266c6f58878d9
ravali-rimmalapudi <83863595+ravali-rimmalapudi@users.noreply.github.com>
2021-09-14 17:33:25 +0530

    feat: Added github actions to release homebrew to github with updated cli version. (#11)
    * feat: Added github actions to release homebrew to github with updated cli version
    
    * Updated script with git conigurations
    
    Co-authored-by: lakshmiravali <lakshmiravali.rimmalapudi@gmail.com>

---
+++ .github/scripts/update-formula.sh
@@ -0,0 +1,24 @@
+version_pattern="[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*"
+formula=$1
+version=$2
+sha=$3
+if [ "$version" == '' ] || [ "$sha" == '' ]; then
+  exit
+fi
+formula_path="Formula/$formula.rb"
+sed -i.bak "s/$formula-v$version_pattern/$formula-v$version/g" "$formula_path"
+sed -i.bak "s/version .*/version $version/" "$formula_path"
+sed -i.bak "s/sha256 .*/sha256 $sha/" "$formula_path"
+echo "Git configurations"
+git config --global user.email "team_interfaces+github@twilio.com"
+git config --global user.name "twilio-dx"
+branch=$(git branch --show-current)
+echo "Current branch: $branch"
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+  echo "There are changes to commit.";
+  git commit -m "Release $version"
+  git push origin "$branch"
+else
+  echo "No changes to commit";
+fi
