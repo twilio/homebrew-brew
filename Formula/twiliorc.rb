d45ff0c6b5d8079966e2e6205df6c7d1fab16c1b
shamantraghav <87780745+shamantraghav@users.noreply.github.com>
2021-11-09 18:01:09 +0530

    fix: correcting the url to pick the package from the rc folder (#23)
    * Update twilio@rc.rb
    
    * Changing formula name
    
    * Update twiliorc.rb
    
    Co-authored-by: sraghav <sraghav@twilio.com>

--- Formula/twiliorc.rb
+++ Formula/twiliorc.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Twiliorc < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v2.27.0-rc.1/twilio-v2.27.0-rc.1.tar.gz"
+  url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v2.27.0-rc.1/twilio-v2.27.0-rc.1.tar.gz"
   version "2.33.0-rc.1"
   sha256 "40bb92c37c57421c2dfa6f97f175781ca8cd42dbc0219a07a7efcec91698cda5"
   depends_on "node"
