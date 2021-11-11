d2762f182dde1fa9079a7c10bd2299375e3bc9e8
ravali-rimmalapudi <83863595+ravali-rimmalapudi@users.noreply.github.com>
2021-11-11 14:50:41 +0530

    Update twiliorc.rb

--- Formula/twiliorc.rb
+++ Formula/twiliorc.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Twiliorc < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v2.34.0-rc.1/twilio-v2.34.0-rc.1.tar.gz"
-  version "2.34.0-rc.1"
-  sha256 "12345678"
+  url "https://twilio-cli-prod.s3.amazonaws.com/channels/rc/twilio-v2.27.0-rc.1/twilio-v2.27.0-rc.1.tar.gz"
+  version "2.33.0-rc.1"
+  sha256 "60b02e5add42fab89065ce54668f123e14201bd2f8708ba162586bf05fab37b3"
   depends_on "node"
 
   def install
