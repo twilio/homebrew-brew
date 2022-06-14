98503d77124ddc920c1db11583e7a765864fc608
Sam Harrison <sharrison@twilio.com>
2022-06-14 14:22:58 -0500

    test: gameday change

--- Formula/twiliodraft@3.1.0-draft.2.rb
+++ Formula/twiliodraft@3.1.0-draft.2.rb
@@ -1,10 +1,10 @@
 require "language/node"
 
-class Twiliodraft < Formula
+class TwiliodraftAT310Draft2 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-stage.s3.amazonaws.com/channels/draft/twilio-v3.1.0-draft.3/twilio-v3.1.0-draft.3.tar.gz"
-  version "3.1.0-draft.3"
+  url "https://twilio-cli-prod.s3.amazonaws.com/channels/draft/twilio-v3.1.0-draft.2/twilio-v3.1.0-draft.2.tar.gz"
+  version "3.1.0-draft.2"
   sha256 "8a43bb3b8a93e0c221d971be30ad96e4ffb38d85bcb1d006671aa57992761e4f"
   depends_on "node"
 
