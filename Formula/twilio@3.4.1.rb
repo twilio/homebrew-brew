1e65fbe65fd92d41480c3064186bf3719ac3fe6c
twilio-dx <team_interfaces+github@twilio.com>
2022-06-16 06:16:10 +0000

    Release 3.4.2

--- Formula/twilio@3.4.1.rb
+++ Formula/twilio@3.4.1.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twilio < Formula
+class TwilioAT341 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v3.4.2/twilio-v3.4.2.tar.gz"
-  version "3.4.2"
-  sha256 "0f2d3448baee770e091b7c20725d05dd1ecdb48ad435ffc2d7c778b58f8c7c76"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v3.4.1/twilio-v3.4.1.tar.gz"
+  version "3.4.1"
+  sha256 "04353a522f83f2c3433860c6442bf12310b1fdc1027de0732d8090f15809c5f9"
   depends_on "node"
 
   def install
