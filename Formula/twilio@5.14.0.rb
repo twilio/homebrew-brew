c4ff3907ae332b01f2c0262d2ca1a743401af172
twilio-dx <team_interfaces+github@twilio.com>
2023-09-21 10:10:56 +0000

    Release 5.15.0

--- Formula/twilio@5.14.0.rb
+++ Formula/twilio@5.14.0.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twilio < Formula
+class TwilioAT5140 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v5.15.0/twilio-v5.15.0.tar.gz"
-  version "5.15.0"
-  sha256 "8099afe05863a7935c4cb403d5f4bf75e529969fef4630d4959bcdf6941cd6ef"
+  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v5.14.0/twilio-v5.14.0.tar.gz"
+  version "5.14.0"
+  sha256 "c92f0f6327fe55a04bf6596377b2f1d06070cb806a02459828c7cd0eac70fdc3"
   depends_on "node"
 
   def install
